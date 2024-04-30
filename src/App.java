7f0f318730694f298c2ead17038243ae1dcff971
Giovanni <usr3855@salesianos-lacuesta.net>
2024-04-30 13:39:26 +0100

    atributos y constructor

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import net.farmatodo.inventario.GeneradorAleatorio;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
     }
 }
--- src/net/farmatodo/clientela/Cliente.java
+++ src/net/farmatodo/clientela/Cliente.java
@@ -1,5 +1,19 @@
 package net.farmatodo.clientela;
 
+import net.farmatodo.inventario.GeneradorAleatorio;
+
 public class Cliente {
     
+    private String nombre;
+    private String dni;
+    private String cuentaBancaria;
+
+    public Cliente (String nombre, String dni, String cuentaBancaria) {
+        this.nombre = GeneradorAleatorio.obtenerNombreAleatorio();
+        this.dni = dni;
+        this.cuentaBancaria = cuentaBancaria;
+    }
+
+    
+
 }
