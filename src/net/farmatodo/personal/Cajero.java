21c4002fc7f6a7d839cdc29e6f60901e9a16dac1
Giovanni <usr3855@salesianos-lacuesta.net>
2024-05-03 09:44:53 +0100

    Creacion atributos y constructor

--- src/net/farmatodo/personal/Cajero.java
+++ src/net/farmatodo/personal/Cajero.java
@@ -2,4 +2,14 @@ package net.farmatodo.personal;
 
 public class Cajero {
     
+    private String nombre;
+    private String id;
+    private int horario;
+
+    public Cajero(String nombre, String id, int horario) {
+        this.nombre = nombre;
+        this.id = id;
+        this.horario = horario;
+    }
+
 }
