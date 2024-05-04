9ed5773cb43825490ea8ab5f08097a201081f57d
Giovanni <usr3855@salesianos-lacuesta.net>
2024-05-04 14:10:07 +0100

    creacion case 1

--- src/net/farmatodo/menu/Menu.java
+++ src/net/farmatodo/menu/Menu.java
@@ -1,7 +1,16 @@
 package net.farmatodo.menu;
 
+import java.util.Scanner;
+
 public class Menu {
+
     public static void mostrarMenu() {
+
+        final Scanner scanner = new Scanner(System.in);
+        Boolean caja;
+        String abrir;
+        String cerrar;
+
         System.out.println("""
                 1. Abrir Caja
                 2. Añadir nuevo cliente a la cola
@@ -12,19 +21,29 @@ public class Menu {
         String menu;
         Boolean bucle = true;
         while (bucle) {
-            
+
             switch (menu) {
                 case "1":
-                    
+
+                    if (caja = false) {
+                        System.out.println("La caja se encuentra cerrada, quieres abrirla?");
+                        abrir = scanner.nextLine();
+                        if (abrir.equalsIgnoreCase("si")) {
+                            caja = true;
+                            System.out.println("La caja ha sido abierta");
+                        }
+                    } else {
+                        System.out.println("La caja ya se encuentra abierta");
+                    }
                     break;
                 case "2":
-                    
+
                     break;
                 case "3":
-                    
+
                     break;
                 case "4":
-                    
+
                     break;
                 case "Q":
                     bucle = false;
@@ -33,4 +52,5 @@ public class Menu {
             }
         }
     }
+
 }
